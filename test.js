2bd2b6f9dbb4e161ed6574f0769be53b7ae26d7d
cancer0627 <1942913930@qq.com>
2016-09-27 23:41:55 +0800

    视频进度条

--- test.js
+++ test.js
@@ -12,40 +12,85 @@
     var btn_music = document.getElementById('btn_music');
     var music_duration = document.getElementById('music_duration');
     var music_current = document.getElementById('music_current');
+    //开始，暂停
     btn_start.onclick = function playpause() {
-        if (video1.paused) {
-            video1.play();
+            if (video1.paused) {
+                video1.play();
+                btn_start.src = "source/stop.png";
+            } else {
+                video1.pause();
+                btn_start.src = "source/start.png";
+            }
+        }
+        //有声，无声
+    btn_music.onclick = function() {
+            if (video1.muted) {
+                video1.muted = false;
+                btn_music.src = "source/musicup.png";
+            } else {
+                video1.muted = true;
+                btn_music.src = "source/musicdown.png";
+            }
+        }
+        //视频开始
+    video1.onplay = function() {
+            console.log("video play");
+            ad1.style.display = "none";
+            btn2.style.display = "none";
             btn_start.src = "source/stop.png";
-        } else {
-            video1.pause();
+        }
+        //视频暂停
+    video1.onpause = function() {
+            ad1.style.display = 'initial';
+            btn2.style.display = 'initial';
             btn_start.src = "source/start.png";
         }
-    }
-    btn_music.onclick = function() {
-        if (video1.muted) {
-            video1.muted = false;
-            btn_music.src = "source/musicup.png";
-        } else {
-            video1.muted = true;
-            btn_music.src = "source/musicdown.png";
-        }
-    }
-    video1.onplay = function() {
-        console.log("video play");
-        ad1.style.display = "none";
-        btn2.style.display = "none";
-        btn_start.src = "source/stop.png";
-    }
-    video1.onpause = function() {
-        ad1.style.display = 'initial';
-        btn2.style.display = 'initial';
-        btn_start.src = "source/start.png";
-    }
+        //广告关闭
     btn1.onclick = function() {
-        ad1.style.display = "none";
-    }
-    btn2.onclick=function (){
+            ad1.style.display = "none";
+        }
+        //开始按钮
+    btn2.onclick = function() {
         video1.play();
     }
-}(window))
 
+    //视频加载时
+    video1.addEventListener('loadedmetadata', function logg() {
+        console.log('loadedmetadata');
+        video1.poster = "source/video1.jpg";
+        show_jindu();
+        jindu_cur();
+    });
+    //视频播放时
+    video1.ontimeupdate = function() {
+        //console.log('video 播放ing');
+        show_jindu();
+        jindu_cur();
+    }
+
+    //进度
+    function show_jindu() {
+        var tim1 = parseInt(video1.currentTime);
+        if (tim1 % 60 < 10) {
+            tim1 = parseInt(tim1 / 60) + ":0" + tim1 % 60;
+        } else {
+            tim1 = parseInt(tim1 / 60) + ":" + tim1 % 60;
+        }
+        var tim2 = parseInt(video1.duration);
+        if (tim2 % 60 < 10) {
+            tim2 = parseInt(tim2 / 60) + ":0" + tim2 % 60;
+        } else {
+            tim2 = parseInt(tim2 / 60) + ":" + tim2 % 60;
+        }
+
+        jindu.innerHTML = tim1 + "/" + tim2;
+    }
+
+    function jindu_cur() {
+        jindu_duration.style.width = (video1.duration / video1.duration) * 100 + '%';
+        jindu_buffer.style.width = ((video1.buffered.length + video1.currentTime) / video1.duration) * 100 + '%';
+        jindu_current.style.width = (video1.currentTime / video1.duration) * 100 + '%';
+    }
+
+
+}(window))
