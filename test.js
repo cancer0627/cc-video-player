5f8474d9a17a1a58efc97be7b9eea3f8450f442e
cui chen <cancer_1942913930@163.com>
2016-12-10 19:46:18 +0800

    代码重构

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-(function() {
+(function () {
     var main_video = document.getElementById('main_video');
     var media_player = document.getElementById('media_player');
 
-    var video1 = document.getElementById('video1');
-    var ad1 = document.getElementById('ad1');
-    var btn_ad = document.getElementById('btn_ad');
-    var btn_sta_lar = document.getElementById('btn_sta_lar');
+    var video = document.getElementById('video');
+    var btn_start_large = document.getElementById('btn_start_large');
     var btn_start = document.getElementById('btn_start');
     var jindu = document.getElementById('jindu');
     var jindu_tiao = document.getElementById('jindu_tiao');
@@ -22,7 +20,8 @@
     var btn_music_down = document.getElementById('btn_music_down');
     var btn_kuaijin = document.getElementById('btn_kuaijin');
     var btn_kuaitui = document.getElementById('btn_kuaitui');
-    var file_button = document.getElementById('file_button');
+    var file_open_btn = document.getElementById('file_open_btn');
+    var file_close_btn = document.getElementById('file_close_btn');
     var file_list = document.getElementById('file_list');
     var file_btn = document.getElementById('file_btn');
     var add_btn = document.getElementById('add_btn');
@@ -31,138 +30,183 @@
     jindu_duration.style.width = '100%';
     music_duration.style.width = "100%";
 
-    file_button.onclick = function() {
-        file_list.classList.toggle("hide_file_list");
+    //进度
+    function show_jindu() {
+        var tim1 = parseInt(video.currentTime);
+        if (tim1 % 60 < 10) {
+            tim1 = parseInt(tim1 / 60) + ":0" + tim1 % 60;
+        } else {
+            tim1 = parseInt(tim1 / 60) + ":" + tim1 % 60;
+        }
+        var tim2 = parseInt(video.duration);
+        if (tim2 % 60 < 10) {
+            tim2 = parseInt(tim2 / 60) + ":0" + tim2 % 60;
+        } else {
+            tim2 = parseInt(tim2 / 60) + ":" + tim2 % 60;
+        }
+        jindu.innerHTML = tim1 + "/" + tim2;
     }
 
-    /*file_list.onmouseout=function (){
-        file_list.classList.toggle("hide_file_list");
-    }*/
+    function jindu_cur() {
+        jindu_duration.style.width = '100%';
+        jindu_buffer.style.left = (video.buffered.start(0) / video.duration) * 100 + '%';
+        jindu_buffer.style.width = ((video.buffered.end(0) - video.buffered.start(0)) / video.duration) * 100 + '%';
+        jindu_current.style.width = (video.currentTime / video.duration) * 100 + '%';
+    }
 
-    add_btn.onclick = function() {
-        var file = file_btn.files[0]
+    //音量条
+    function music_cur() {
+        music_duration.style.width = "100%";
+        music_current.style.width = (video.volume * 100) + '%';
+    }
+
+    function musicbtn() {
+        if (video.volume > 0.66 && video.volume <= 1) {
+            btn_music_con.src = "source/musicup3.png";
+        } else if (video.volume > 0.33 && video.volume <= 0.66) {
+            btn_music_con.src = "source/musicup2.png";
+        } else if (video.volume > 0 && video.volume <= 0.33) {
+            btn_music_con.src = "source/musicup1.png";
+        } else {
+            btn_music_con.src = "source/musicdown.png";
+        }
+    }
+
+    /*---------------------------------------------------------------------------------------------------------------*/
+    //本地文件加载
+    file_open_btn.onclick = function () {
+        file_list.classList.toggle("hide_file_list");
+        file_open_btn.classList.toggle("hide_file_list");
+    };
+    file_close_btn.onclick = function () {
+        file_list.classList.toggle("hide_file_list");
+        file_open_btn.classList.toggle("hide_file_list");
+    };
+    add_btn.onclick = function () {
+        var file = file_btn.files[0];
         var url = URL.createObjectURL(file);
         console.log(url);
-        video1.src = url;
-    }
+        video.src = url;
+    };
 
-    //开始，暂停，快进
+    //视频加载时
+    video.addEventListener('loadedmetadata', function logg() {
+        show_jindu();
+        jindu_cur();
+        music_cur();
+        musicbtn();
+    });
+
+    //视频播放时
+    video.ontimeupdate = function () {
+        show_jindu();
+        jindu_cur();
+        music_cur();
+        musicbtn();
+    };
+
+    //视频开始
+    video.onplay = function () {
+        console.log("video play");
+        btn_start_large.style.display = "none";
+        btn_start.src = "source/stop.png";
+    };
+
+    //视频暂停
+    video.onpause = function () {
+        btn_start_large.style.display = 'initial';
+        btn_start.src = "source/start.png";
+    };
+
+    //开始按钮
+    btn_start_large.onclick = function () {
+        video.play();
+    };
+
+    //开始，暂停，快进，快退
     btn_start.onclick = function playpause() {
-        if (video1.paused) {
-            video1.play();
+        if (video.paused) {
+            video.play();
             btn_start.src = "source/stop.png";
         } else {
-            video1.pause();
+            video.pause();
             btn_start.src = "source/start.png";
         }
-    }
+    };
 
-    video1.ondblclick = function playpause() {
-        if (video1.paused) {
-            video1.play();
+    video.ondblclick = function playpause() {
+        if (video.paused) {
+            video.play();
             btn_start.src = "source/stop.png";
         } else {
-            video1.pause();
+            video.pause();
             btn_start.src = "source/start.png";
         }
-    }
+    };
 
-    btn_kuaijin.onclick = function() {
-        video1.currentTime += 5;
-    }
+    btn_kuaijin.onclick = function () {
+        video.currentTime += 5;
+    };
 
-    btn_kuaitui.onclick = function() {
-        video1.currentTime -= 5;
-    }
+    btn_kuaitui.onclick = function () {
+        video.currentTime -= 5;
+    };
 
     //有声，无声，增加音量，减小音量
-    btn_music_con.onclick = function() {
-        if (video1.volume != 0) {
-            video1.volume = 0;
+    btn_music_con.onclick = function () {
+        if (video.volume != 0) {
+            video.volume = 0;
             musicbtn();
             music_cur();
         } else {
-            video1.volume = 1;
+            video.volume = 1;
             musicbtn();
             music_cur();
         }
-    }
-
-    btn_music_up.onclick = function() {
-        if (video1.volume >= 0.9) {
-            video1.volume = 1;
+    };
+    btn_music_up.onclick = function () {
+        if (video.volume >= 0.9) {
+            video.volume = 1;
         } else {
-            video1.volume += 0.1;
+            video.volume += 0.1;
         }
         music_cur();
         musicbtn();
-    }
-
-    btn_music_down.onclick = function() {
-        if (video1.volume <= 0.1) {
-            video1.volume = 0;
+    };
+    btn_music_down.onclick = function () {
+        if (video.volume <= 0.1) {
+            video.volume = 0;
         } else {
-            video1.volume -= 0.1;
+            video.volume -= 0.1;
         }
         music_cur();
         musicbtn();
-    }
-
-    btn_music.onmouseover = function() {
+    };
+    btn_music.onmouseover = function () {
         btn_music_up.style.display = "block";
         btn_music_down.style.display = "block";
         btn_music.style.marginTop = "-60px";
-    }
-
-    btn_music.onmouseout = function() {
+    };
+    btn_music.onmouseout = function () {
         btn_music_up.style.display = "none";
         btn_music_down.style.display = "none";
         btn_music.style.marginTop = "0px";
-    }
-
-    //视频开始
-    video1.onplay = function() {
-        console.log("video play");
-        ad1.style.display = "none";
-        btn_sta_lar.style.display = "none";
-        btn_start.src = "source/stop.png";
-    }
-
-    //视频暂停
-    video1.onpause = function() {
-        ad1.style.display = 'initial';
-        btn_sta_lar.style.display = 'initial';
-        btn_start.src = "source/start.png";
-    }
-
-    //广告关闭
-    btn_ad.onclick = function() {
-        ad1.style.display = "none";
-    }
-
-    //开始按钮
-    btn_sta_lar.onclick = function() {
-        video1.play();
-    }
+    };
 
     //全屏
-    btn_quanping.onclick = function() {
+    btn_quanping.onclick = function () {
         if (btn_quanping.src.match("quanping1.png")) {
-            main_video.style.width = "80%";
-            main_video.style.margin = "80px auto";
-            media_player.style.position = "initial";
+            main_video.classList.toggle('fullscreen');
+            media_player.classList.toggle('fullscreen');
             btn_quanping.src = "source/quanping0.png";
             exitFull();
         } else {
-            main_video.style.width = "100%";
-            main_video.style.height = "100%";
-            main_video.style.margin = "0";
-            media_player.style.position = "absolute";
+            main_video.classList.toggle('fullscreen');
+            media_player.classList.toggle('fullscreen');
             btn_quanping.src = "source/quanping1.png";
             requestFullScreen(document.documentElement);
         }
-
-    }
+    };
 
     function requestFullScreen(element) {
         // 判断各种浏览器，找到正确的方法
@@ -182,8 +226,6 @@
 
     //退出全屏 判断浏览器种类
     function exitFull() {
-
-
         // 判断各种浏览器，找到正确的方法
         var exitMethod = document.exitFullscreen || //W3C
             document.mozCancelFullScreen || //Chrome等
@@ -197,146 +239,41 @@
                 wscript.SendKeys("{F11}");
             }
         }
-
     }
 
-    //视频加载时
-    video1.addEventListener('loadedmetadata', function logg() {
-        /*video1.poster = "https://media.w3.org/2010/05/sintel/poster.png";*/
-        show_jindu();
-        jindu_cur();
-        music_cur();
-        musicbtn();
-        console.log(video1.buffered.start(0));
-    })
-
-    //视频播放时
-    video1.ontimeupdate = function() {
-        show_jindu();
-        jindu_cur();
-        music_cur();
-        musicbtn();
-        console.log(video1.buffered.end(0));
-    }
-
-    //进度
-    function show_jindu() {
-        var tim1 = parseInt(video1.currentTime);
-        if (tim1 % 60 < 10) {
-            tim1 = parseInt(tim1 / 60) + ":0" + tim1 % 60;
-        } else {
-            tim1 = parseInt(tim1 / 60) + ":" + tim1 % 60;
-        }
-        var tim2 = parseInt(video1.duration);
-        if (tim2 % 60 < 10) {
-            tim2 = parseInt(tim2 / 60) + ":0" + tim2 % 60;
-        } else {
-            tim2 = parseInt(tim2 / 60) + ":" + tim2 % 60;
-        }
-        jindu.innerHTML = tim1 + "/" + tim2;
-    }
-
-    function jindu_cur() {
-        jindu_duration.style.width = '100%';
-        jindu_buffer.style.left = (video1.buffered.start(0) / video1.duration) * 100 + '%';
-        jindu_buffer.style.width = ((video1.buffered.end(0) - video1.buffered.start(0)) / video1.duration) * 100 + '%';
-        jindu_current.style.width = (video1.currentTime / video1.duration) * 100 + '%';
-    }
-
-    //音量条
-    function music_cur() {
-        music_duration.style.width = "100%";
-        music_current.style.width = (video1.volume * 100) + '%';
-    }
-
-    function musicbtn() {
-        if (video1.volume > 0.66 && video1.volume <= 1) {
-            btn_music_con.src = "source/musicup3.png";
-        } else if (video1.volume > 0.33 && video1.volume <= 0.66) {
-            btn_music_con.src = "source/musicup2.png";
-        } else if (video1.volume > 0 && video1.volume <= 0.33) {
-            btn_music_con.src = "source/musicup1.png";
-        } else {
-            btn_music_con.src = "source/musicdown.png";
-        }
-    }
-
-    //音量拖动
+    /*音量，进度条--拖动，点击效果-----------------------------------------------------------------------------------*/
     var x_sta, x_end, x_cur, x_dur, x_len;
+    //音量拖动
+    function music_drag(event) {
+        x_end = event.clientX;
+        x_len = (x_cur - x_sta + x_end) / x_dur;
+        if (x_len <= 0) {
+            video.volume = 0;
+        } else if (x_len >= 1) {
+            video.volume = 1;
+        } else {
+            video.volume = x_len;
+        }
+        music_current.style.width = video.volume * 100 + "%";
+        musicbtn();
+    }
 
-    music_button.ondragstart = function() {
+    music_button.ondragstart = function () {
         x_sta = event.clientX;
         x_cur = music_current.clientWidth;
         x_dur = music_duration.clientWidth;
-    }
-
-    music_button.ondrag = function(event) {
-        x_end = event.clientX;
-        x_len = (x_cur - x_sta + x_end) / x_dur;
-        if (x_len <= 0) {
-            video1.volume = 0;
-        } else if (x_len >= 1) {
-            video1.volume = 1;
-        } else {
-            video1.volume = x_len;
-        }
-        music_current.style.width = video1.volume * 100 + "%";
-        musicbtn();
-    }
-
-    music_button.ondragend = function(event) {
-        x_end = event.clientX;
-        x_len = (x_cur - x_sta + x_end) / x_dur;
-        if (x_len <= 0) {
-            video1.volume = 0;
-        } else if (x_len >= 1) {
-            video1.volume = 1;
-        } else {
-            video1.volume = x_len;
-        }
-        music_current.style.width = video1.volume * 100 + "%";
-        musicbtn();
-    }
-
-    //音量点击切换
-    music_duration.onclick = function(event) {
-        x_end = event.layerX;
-        x_dur = music_duration.clientWidth;
-        x_len = x_end / x_dur;
-        if (x_len <= 0) {
-            video1.volume = 0;
-        } else if (x_len >= 1) {
-            video1.volume = 1;
-        } else {
-            video1.volume = x_len;
-        }
-        music_current.style.width = video1.volume * 100 + "%";
-        musicbtn();
-    }
-
-    music_current.onclick = function(event) {
-        x_end = event.layerX;
-        x_dur = music_duration.clientWidth;
-        x_len = x_end / x_dur;
-        if (x_len <= 0) {
-            video1.volume = 0;
-        } else if (x_len >= 1) {
-            video1.volume = 1;
-        } else {
-            video1.volume = x_len;
-        }
-        music_current.style.width = video1.volume * 100 + "%";
-        musicbtn();
-    }
+    };
+    music_button.ondrag = music_drag;
+    music_button.ondragend = music_drag;
 
     //进度拖动
-    jindu_button.ondragstart = function(event) {
+    jindu_button.ondragstart = function (event) {
         x_sta = event.clientX;
         x_cur = jindu_current.clientWidth;
         x_dur = jindu_duration.clientWidth;
-    }
+    };
 
-    jindu_button.ondrag = function(event) {
+    jindu_button.ondrag = function (event) {
         x_end = event.clientX;
         x_len = (x_cur - x_sta + x_end) / x_dur;
         if (x_len <= 0) {
@@ -344,13 +281,12 @@
         } else if (x_len >= 1) {
             x_len = 1;
         } else {
-            x_len = x_len;
         }
-        video1.currentTime = x_len * video1.duration;
+        video.currentTime = x_len * video.duration;
         jindu_cur();
-    }
+    };
 
-    jindu_button.ondragend = function(event) {
+    jindu_button.ondragend = function (event) {
         x_end = event.clientX;
         x_len = (x_cur - x_sta + x_end) / x_dur;
         if (x_len <= 0) {
@@ -358,14 +294,32 @@
         } else if (x_len >= 1) {
             x_len = 1;
         } else {
-            x_len = x_len;
         }
-        video1.currentTime = x_len * video1.duration;
+        video.currentTime = x_len * video.duration;
         jindu_cur();
+    };
+
+    //音量点击切换
+    function music_click(event) {
+        x_end = event.layerX;
+        x_dur = music_duration.clientWidth;
+        x_len = x_end / x_dur;
+        if (x_len <= 0) {
+            video.volume = 0;
+        } else if (x_len >= 1) {
+            video.volume = 1;
+        } else {
+            video.volume = x_len;
+        }
+        music_current.style.width = video.volume * 100 + "%";
+        musicbtn();
     }
+
+    music_duration.onclick = music_click;
+    music_current.onclick = music_click;
 
     //进度点击切换
-    jindu_duration.onclick = function(event) {
+    function jindu_click(event) {
         x_end = event.layerX;
         x_dur = jindu_duration.clientWidth;
         x_len = x_end / x_dur;
@@ -374,40 +328,13 @@
         } else if (x_len >= 1) {
             x_len = 1;
         } else {
-            x_len = x_len;
         }
-        video1.currentTime = x_len * video1.duration;
+        video.currentTime = x_len * video.duration;
         jindu_cur();
     }
 
-    jindu_buffer.onclick = function(event) {
-        x_end = event.layerX;
-        x_dur = jindu_duration.clientWidth;
-        x_len = x_end / x_dur;
-        if (x_len <= 0) {
-            x_len = 0;
-        } else if (x_len >= 1) {
-            x_len = 1;
-        } else {
-            x_len = x_len;
-        }
-        video1.currentTime = x_len * video1.duration;
-        jindu_cur();
-    }
+    jindu_duration.onclick = jindu_click;
+    jindu_buffer.onclick = jindu_click;
+    jindu_current.onclick = jindu_click;
 
-    jindu_current.onclick = function(event) {
-        x_end = event.layerX;
-        x_dur = jindu_duration.clientWidth;
-        x_len = x_end / x_dur;
-        if (x_len <= 0) {
-            x_len = 0;
-        } else if (x_len >= 1) {
-            x_len = 1;
-        } else {
-            x_len = x_len;
-        }
-        video1.currentTime = x_len * video1.duration;
-        jindu_cur();
-    }
-
-}(window))
+}(window));
